c50b54de871d2fffb3af831fb2ffa39dbf83b4b2
Gabriele Fiacconi <80699087+gfiacconi@users.noreply.github.com>
2022-10-14 19:47:49 +0200

    Update settings.py

--- PreRental/settings.py
+++ PreRental/settings.py
@@ -101,7 +101,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -109,8 +109,6 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
